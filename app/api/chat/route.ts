6ab9f595a68db4d3b386a6155dc79e4d21c4ea0b
DeeStroKeR <deestrok@gmail.com>
2025-02-21 14:46:56 +0100

    Add files via upload

---
+++ app/api/chat/route.ts
@@ -0,0 +1,15 @@
+import { bedrock } from "@ai-sdk/amazon-bedrock";
+import { streamText } from "ai";
+ 
+export const maxDuration = 2000;
+ 
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+  const result = streamText({
+    // model: bedrock("anthropic.claude-3-5-sonnet-20240620-v1:0"),
+    model: bedrock("anthropic.claude-3-haiku-20240307-v1:0"),
+    messages,
+  });
+  console.log(result.toDataStreamResponse());
+  return result.toDataStreamResponse();
+}
